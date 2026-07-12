2b05086c268b6c984800c75183dbdccdd2876635
Francisco Dias <49660544+FranciscoFHDias@users.noreply.github.com>
2026-07-12 20:50:52 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default function HomePage() {
             href="/about"
             className="inline-flex items-center justify-center rounded-md bg-indigo-500 px-4 py-2 text-sm font-semibold text-white shadow-sm transition hover:bg-indigo-400"
           >
-            Go to about me
+            Go to about
           </Link>
         </div>
       </div>
